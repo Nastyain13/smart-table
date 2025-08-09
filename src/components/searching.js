881e852dbc8a46b8e9e5d2a9b495b35199aa6037
NASTYA <iwanowa.anastasiya2012@yandex.ru>
2025-08-09 21:45:32 +0300

    попытка3

--- src/components/searching.js
+++ src/components/searching.js
@@ -2,16 +2,13 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
- const compare = createComparison(
-        rules.skipEmptyTargetValues, rules.searchMultipleFields (searchField, ['date', 'customer', 'seller'], false))
+    const compare = createComparison(
+        ['skipEmptyTargetValues'], [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)])
 
 
-        // @todo: #5.2 — применить компаратор
-        return function filterData(data, state) {
-       
-    return data.filter(row => compare(row, state));
+    // @todo: #5.2 — применить компаратор
+    return function filterData(data, state) {
+
+        return data.filter(row => compare(row, state));
     };
 }
-
-     
- 
